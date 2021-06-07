43911db29aaf5b3685a734d6eb3da30bd2fb3cb0
iradbouzidi <irad.bouzidi@esprit.tn>
2021-06-07 13:56:49 -0700

    mqtt

--- back-end/app.py
+++ back-end/app.py
@@ -91,7 +91,7 @@ def PUBLISH_USER(message):
     client.connect(iot_hub_name+".azure-devices.net", port=8883)
 
     # Publish
-    time.sleep(1)
+    #time.sleep(1)
     # exp = datetime.datetime.utcnow()
     jsonstring = {
         "QUERY": message
@@ -101,8 +101,8 @@ def PUBLISH_USER(message):
         device_id=device_id), payload=data_out1, qos=1, retain=False)
     print("Publishing on devices/" + device_id +
           "/messages/events/", data_out1)
-    time.sleep(5)
-    client.disconnect()
+    #time.sleep(5)
+    # client.disconnect()
 
     # Subscribe
     """client.on_message = on_message
