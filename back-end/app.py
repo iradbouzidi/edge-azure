d07758018095379cc729cb92c9f450202d750f19
iradbouzidi <irad.bouzidi@esprit.tn>
2021-06-07 13:38:45 -0700

    mqtt

--- back-end/app.py
+++ back-end/app.py
@@ -111,63 +111,6 @@ def PUBLISH_USER(message):
         "devices/{device_id}/messages/devicebound/#".format(device_id=device_id))
     client.loop_forever()"""
 
-
-"""
-def PUBLISH_USER(message):
-
-    def on_subscribe(client, userdata, mid, granted_qos):
-        print('Subscribed for m' + str(mid))
-
-    def on_connect(client, userdata, flags, rc):
-        print("Connected with result code "+str(rc))
-
-    def on_message(client, userdata, message):
-        print("Received message '" + str(message.payload) + "' on topic '" +
-              message.topic + "' with QoS " + str(message.qos))
-
-    def on_log(client, userdata, level, buf):
-        print("log: ", buf)
-
-    device_id = "rpi-core"  # Add device id
-    iot_hub_name = "MWIoTHub"  # Add iot hub name
-    # sas_token = "SharedAccessSignature sr=MWIoTHub.azure-devices.net%2Fdevices%2Frpi-core&sig=VFRsENBd7LnjPlIdTyJRIN%2BiiGjLW%2Fht1vBjiz1ytQI%3D&se=1623091322"  # Add sas token string
-    sas_token = "SharedAccessSignature sr=MWIoTHub.azure-devices.net%2Fdevices%2Frpi-core&sig=3%2BMEKm1mlBVtIGJs%2FvYyr%2BJFXt1jhPDVmqUxkBVBeJ8%3D&se=1623170825"
-
-    client = mqtt.Client(client_id=device_id,
-                         protocol=mqtt.MQTTv311,  clean_session=False)
-
-    client.on_log = on_log
-    client.tls_set_context(context=None)
-
-    # Set up client credentials
-    username = "{}.azure-devices.net/{}/api-version=2018-06-30".format(
-        iot_hub_name, device_id)
-    client.username_pw_set(username=username, password=sas_token)
-
-    # Connect to the Azure IoT Hub
-    client.on_connect = on_connect
-    client.connect(iot_hub_name+".azure-devices.net", port=8883)
-
-    # Publish
-    # time.sleep(1)
-    # exp = datetime.datetime.utcnow()
-    jsonstring = {
-        "QUERY": message
-    }
-    data_out1 = json.dumps(jsonstring)
-    client.publish("devices/{device_id}/messages/events/".format(
-        device_id=device_id), payload=data_out1, qos=1, retain=False)
-    print("Publishing on devices/" + device_id +
-          "/messages/events/", data_out1)
-    # time.sleep(5)
-    # Subscribe
-    client.on_message = on_message
-    client.on_subscribe = on_subscribe
-    client.subscribe(
-        "devices/{device_id}/messages/devicebound/#".format(device_id=device_id))
-    client.loop_forever()
-"""
-
 # * --------------------  ROUTES ------------------- *
 # * ---------- Test server ---------- *
 
