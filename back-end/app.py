4de726b025234d02e137d7105e49781b8b45bc8f
iradbouzidi <irad.bouzidi@esprit.tn>
2021-11-21 18:03:24 +0000

    done

--- back-end/app.py
+++ back-end/app.py
@@ -78,8 +78,11 @@ def PUBLISH_USER(message):
 
     # Set up client credentials
     username = "{}.azure-devices.net/{}/api-version=2018-06-30".format(iot_hub_name, device_id)
-    client.username_pw_set(username=username, password=generate_sas_token(iot_hub_name + ".azure-devices.net/devices/" + device_id, device_key, device_id))
+    #SharedAccessSignature = "SharedAccessSignature sr=kevinsayIoT.azure-devices.net%2fdevices%2fpython&sig=ON2EYJR7R7ILlA1ChUC84rJSMWEBe%2fCJrBdb8lcIr7A%3d&se=1505515292"
+    SharedAccessSignature = "SharedAccessSignature sr=MWIoTHub.azure-devices.net%2Fdevices%2Frpi-core%2Fmodules%2FBackEnd&sig=VPew8x1%2BylW5IJwjc6HOK1YbmYlADuyfZbGcOk1j7WI%3D&se=1637521273"
+    #client.username_pw_set(username=username, password=generate_sas_token(iot_hub_name + ".azure-devices.net/devices/" + device_id, device_key, device_id))
 
+    client.username_pw_set(username=username, password=SharedAccessSignature)
     # Connect to the Azure IoT Hub
     client.on_connect = on_connect
     client.connect(iot_hub_name+".azure-devices.net", port=8883)
